00db20b33e3559a3953d6b6beab1a327786dc962
Ossten <osten@protonmail.com>
2020-05-08 20:04:53 +0200

    1st commit

---
+++ Palindrome/palindrome_v1.py
@@ -0,0 +1,29 @@
+import math
+import time
+start = time.time()
+
+def is_palindrome_v1(s):
+    #print('Please insert the string: ')
+    #s = input()
+    return reverse(s) == s
+
+def reverse(s):
+    rev = ''
+    for chr in s: 
+        rev = chr+rev
+    return rev
+
+
+if  __name__ == '__main__':
+    print('In version 1, the module hame is: ', __name__)
+    word  = input('Enter a word: ')
+    if is_palindrome_v1(word):
+        print(word,' is palindrome.')
+    else:
+        print(word,' is not a palindrome.')
+
+    # is_palindrome_v1('noon')
+    # is_palindrome_v1('racecar')
+    # is_palindrome_v1('dented')
+
+    print('Executed in : ',time.time() - start,'s.')
